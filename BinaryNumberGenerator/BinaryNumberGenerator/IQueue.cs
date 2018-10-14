e009188af0553eeab1a8726434bf5eba30ba96f5
autumngreenley <pokemonut@comcast.net>
2018-10-14 13:22:49 -0700

    Adding comments and also the IQueue file

---
+++ BinaryNumberGenerator/BinaryNumberGenerator/IQueue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNumberGenerator
+{
+    public interface IQueue
+    {
+        /// <summary>
+        /// Add an element to the rear of the queue
+        /// </summary>
+        /// <param name="Element">The element to add</param>
+        /// <returns>The element that was enqueued</returns>
+        T Push(T Element);
+
+        /// <summary>
+        /// Remove and return the front element.
+        /// </summary>
+        /// <returns>The front element</returns>
+        T Pop();
+
+        /// <summary>
+        /// Test if the queue is empty
+        /// </summary>
+        /// <returns>True if the queue is empty; otherwise false</returns>
+        Boolean IsEmpty();
+    }
+}
--- BinaryNumberGenerator/BinaryNumberGenerator/Node.cs
+++ BinaryNumberGenerator/BinaryNumberGenerator/Node.cs
@@ -11,6 +11,12 @@ namespace BinaryNumberGenerator
         public T Data;
         public Node<T> Next;
 
+        /// <summary>
+        /// Singly linked node object
+        /// </summary>
+        /// <param name="Data">What's in the node</param>
+        /// <param name="Next">The next node in the linked list</param>
+        
         public Node(T Data, Note<T> Next)
         {
             this.Data = Data;
